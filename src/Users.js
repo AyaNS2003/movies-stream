f24106b76c60c2c699d5121077172aed49b8b04c
AyaNS2003 <aya.sawalha.2003@gmail.com>
2024-08-29 18:46:18 +0300

    feat (leftside) : add users part

--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,12 @@
 const Users = () => {
     return ( 
         <div className="Users">
+            <div className="usersSmallDiv">
+                <img src='/img/person1.jpeg' className="person"/>
+                <img src='/img/person2.jpeg' className="person"/>
+                <img src='/img/person3.jpeg' className="person"/>
+                <img src='/img/plus.png' width={24} className="plusIcon"/>
+            </div>
             
         </div>
      );
