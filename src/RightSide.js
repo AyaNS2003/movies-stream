35a0a9b784b51c4c2583a6dc70300e2d70e4eb05
AyaNS2003 <aya.sawalha.2003@gmail.com>
2024-08-29 20:59:07 +0300

    feat (rightside) : adding genre with styling

--- src/RightSide.js
+++ src/RightSide.js
@@ -1,9 +1,11 @@
+import Genre from "./Genre";
 import MediaService from "./MediaService";
 
 const RightSide = () => {
     return ( 
         <div className="RightSide">
             <MediaService/>
+            <Genre/>
         </div>
      );
 }
