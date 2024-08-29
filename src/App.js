bd5096f0cd06c416534abf37a064b013a74332f3
AyaNS2003 <aya.sawalha.2003@gmail.com>
2024-08-29 08:45:06 +0300

    feat: creating main window

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Window from './Window';
+
+function App() {
+  const styling = {
+    background: 'linear-gradient(to right, #FFA41C, #1D1D1D)',
+    height: '100vh',
+    margin: 0,
+    position: 'relative',
+    top: 0,
+    left: 0
+  };
+
+  return (
+    <div className="App"style={styling}>
+      <Window/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Window.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Window = () => {
+  const styling = {
+    width: '80%',
+    height: '90%',
+    position : 'absolute',
+    left: '10%',
+    top: '5%',
+    backgroundColor: '#181715',
+    borderRadius: '20px',
+  };
+  
+  return (
+    <div className="Window" style={styling}>
+
+    </div>
+  );
+};
+
+export default Window;
